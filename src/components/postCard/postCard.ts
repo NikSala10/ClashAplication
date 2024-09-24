904c592d94e22c02d8564228bd3d1cd1df22ed86
NikSala10 <salamancanikol10@gmail.com>
2024-09-24 11:33:27 -0500

    cardwithdata

---
+++ src/components/postCard/postCard.ts
@@ -0,0 +1,123 @@
+export enum Attribute  { 
+    'imguser' = 'imguser',
+    'name' = 'name',
+    'username' = 'username',
+    'category' = 'category',
+    'description' = 'description',
+    'image' = 'image',
+    'timeposted' = 'timeposted',
+    'hashtags' = 'hashtags',
+    'state' = 'state',
+    'likes' =  'likes',
+    'comments' = 'comments',
+    'favorites' = 'favorites'
+}
+
+class PostCard extends HTMLElement  {
+    imguser?: string;
+    name?: string;
+    username?: string;
+    category?: string;
+    description?: string;
+    image?: string;
+    timeposted?: string;
+    hashtags?: string ;
+    state?: string;
+    likes? : number;
+    comments?: number;
+    favorites?: number;
+
+    constructor()  {
+        super();
+        this.attachShadow( {mode: 'open'})
+    }
+
+    static get observedAttributes() {
+        return Object.values(Attribute);
+    }
+
+    attributeChangedCallback(propName : Attribute, oldValue: string | undefined, newValue: string | undefined) {
+        switch (propName) {
+            case Attribute.state:
+                this.state = newValue;  
+            break;
+            case Attribute.likes:
+                this.likes = newValue ? Number(newValue) : undefined;
+                break;
+            case Attribute.comments:
+                this.comments = newValue ? Number(newValue) : undefined;
+                break;
+            case Attribute.favorites:
+                this.favorites = newValue ? Number(newValue) : undefined;
+                break;
+            default:
+                this[propName] = newValue;
+                break;
+        }
+        this.render();
+    }
+    connectedCallback() { 
+        console.log(this);
+        
+        this.render();
+
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="../src/components/postCard/postCard.css">
+           
+                <div class= "card">
+                    <div class="part-user">
+                        <div  circle-img>
+                            <img id="img-user" src="${this.imguser ? this.imguser : 'Not found'}">
+                        </div>
+                        
+                        <div class="nacate">
+                            <h4 class="name">${this.name ? this.name : 'Not found'}</h4>
+                            <p class="category">${this.category ? this.category : 'Not found'}</p>
+                        </div>
+                        
+                        <h4 class="username">${this.username ? this.username : 'Not found'}</h4>
+                        <p class="state">${this.state ? this.state : 'Not found'}</p>
+                        <p class="time-post"> ${this.timeposted ? this.timeposted : 'Not found'}</p>
+                    </div>
+                    <p class="description">${this.description ? this.description : 'Not found'}</p>
+                    <p class="hashtags">${this.hashtags ? this.hashtags : 'Not found'}</p>
+                    <img class="img-post" src="${this.image ? this.image : 'Not found'}">
+                    <div class="post-icons">
+                        <div class="counter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                            </svg>
+                            <p>10</p>
+                        </div>
+                        <div class="counter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chat-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            </svg>
+                            <p>10</p>
+                        </div>
+                        <div class="counter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+                            </svg>
+                            <p>10</p>
+                        </div>
+                        <div class="counter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+                            </svg>
+                            <p>10</p>
+                        </div>
+                    </div>
+                </div>
+          
+            `;
+        }
+    }
+};
+
+customElements.define('card-post',PostCard);
+export default PostCard;
--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -1,25 +1,25 @@
-// import  {workers} from './data/data'
+import  {postsUsers} from './data/dataPost'
 import './components/nav/nav'
 import * as components from './components/indexPadre' ;
-// import Filter, {Attribute} from './components/Mycomponent/component';
+import PostCard, {Attribute} from './components/postCard/postCard';
 class AppContainer extends HTMLElement  {
-    // workersList: Filter[] = []
+    userPostList: PostCard[] = []
     constructor()  {
         super();
         this.attachShadow( {mode: 'open'});
-        // workers.forEach(worker=>  {
-        //     const workerFilter = this.ownerDocument.createElement("component-container") as Filter;
-        //     workerFilter.setAttribute(Attribute.image, worker.image);
-        //     workerFilter.setAttribute(Attribute.name, worker.name);
-        //     workerFilter.setAttribute(Attribute.uid, String(worker.id));
-        //     workerFilter.setAttribute(Attribute.age, String(worker.age));
-        //     workerFilter.setAttribute(Attribute.gender, worker.gender);
-        //     workerFilter.setAttribute(Attribute.area, worker.jobDetails.area);
-        //     workerFilter.setAttribute(Attribute.position, worker.jobDetails.position);
-        //     workerFilter.setAttribute(Attribute.timeincompany, String(worker.jobDetails.timeInCompany));
-        //     workerFilter.setAttribute(Attribute.experience, String(worker.jobDetails.experience));
-        //     this.workersList.push(workerFilter);
-        // })
+        postsUsers.forEach(user=>  {
+            const userPostCards = this.ownerDocument.createElement("card-post") as PostCard;
+            userPostCards.setAttribute(Attribute.imguser, String( user.imgUser));
+            userPostCards.setAttribute(Attribute.name, user.name);
+            userPostCards.setAttribute(Attribute.username, user.username);
+            userPostCards.setAttribute(Attribute.category, user.category);
+            userPostCards.setAttribute(Attribute.state, user.state);
+            userPostCards.setAttribute(Attribute.description, user.description);
+            userPostCards.setAttribute(Attribute.image, user.image);
+            userPostCards.setAttribute(Attribute.timeposted, String( user.timePosted));
+            userPostCards.setAttribute(Attribute.hashtags, user.hashtags.join(", "));
+            this.userPostList.push(userPostCards);
+        })
     }
 
     connectedCallback() {
@@ -31,13 +31,23 @@ class AppContainer extends HTMLElement  {
     render()  {
         if (this.shadowRoot) {
             this.shadowRoot.innerHTML = `
+             <link rel="stylesheet" href="/src/components/postCard/postCard.css">
            
             <nav-component></nav-component>
+            <section class="containers">
+                <div class="container-postcards"></div>
+            </section>
+            
             `;
-
-        };
-        
-        
+          
+            const containerPost = this.shadowRoot?.querySelector('.container-postcards')
+            
+            this.userPostList.forEach((post) =>  {
+                containerPost?.appendChild(post);
+            });
+           
+       
+        }
        
     }
 
