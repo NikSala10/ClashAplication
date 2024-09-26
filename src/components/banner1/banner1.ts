09b3f249b00e0135b4b48eb58be17845f3dcfd78
yeliani <yelianibarbosa@gmail.com>
2024-09-25 19:00:13 -0500

    data

---
+++ src/components/banner1/banner1.ts
@@ -0,0 +1,54 @@
+export enum Attribute  {
+    'image' = 'image',
+    'uid' = 'uid',
+    
+}
+
+class Banner1 extends HTMLElement  {
+    image?: string;
+    uid?: number;
+   
+    constructor()  {
+        super();
+        this.attachShadow( {mode: 'open'})
+    }
+
+    static get observedAttributes() {
+        return Object.values(Attribute);
+    }
+
+    attributeChangedCallback(propName : Attribute, oldValue: string | undefined, newValue: string | undefined) {
+        
+        switch (propName) {
+            case Attribute.image:
+                this.image = newValue; 
+                break;
+            
+        }
+        this.render();
+    }
+    connectedCallback() { 
+        this.render();
+        
+        
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="../src/components/myComponent/component.css">
+           
+                <div class= "cardWorker">
+                    <div id="image">
+                        <img id="img" src="${this.image ? this.image : 'Not found'}">
+                    </div>
+                
+                </div>
+          
+            `;
+        }
+    }
+};
+
+customElements.define('component-container',Banner1);
+export default Banner1;
--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -1,37 +1,43 @@
 import * as components from './components/indexPadre'
+import  {imageArray} from './components/data/data'
 import './components/post1/post1'
 import './components/footer/footer'
+import './components/banner1/banner1'
+import Banner1, {Attribute} from './components/banner1/banner1';
+
 class AppContainer extends HTMLElement  {
-    // workersList: Filter[] = []
-    constructor()  {
+    imagesBanner: Banner1[] = [];
+
+    constructor() {
         super();
-        this.attachShadow( {mode: 'open'});
-      
+        this.attachShadow({ mode: 'open' });
+        imageArray.forEach(img => {
+            const imageArrayBanner1 = this.ownerDocument.createElement("component-container") as Banner1;
+            imageArrayBanner1.setAttribute(Attribute.image, img.img);
+            this.imagesBanner.push(imageArrayBanner1);
+        });
     }
 
     connectedCallback() {
-       
         this.render();
-        
     }
 
-    render()  {
+    render() {
         if (this.shadowRoot) {
             this.shadowRoot.innerHTML = `
-            <post-1></post-1>
-            <foo-ter></foo-ter>
-
-           
-           
+            <link rel="stylesheet" href="../src/Components/index.css">
+                <post-1></post-1>
+                <div class="container"></div>
+                <foo-ter></foo-ter>
             `;
+        }
 
-        };
-        
-        
-       
+        const container = this.shadowRoot?.querySelector('.container');
+        this.imagesBanner.forEach(img => {
+            container?.appendChild(img);
+        });
     }
-
 }
 
-customElements.define('app-container',AppContainer);
-export default AppContainer ;
+customElements.define('app-container', AppContainer);
+export default AppContainer;
