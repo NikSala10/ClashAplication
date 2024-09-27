b7ca6af867bd7da6bc34367ff9f712bafa6edc62
yeliani <yelianibarbosa@gmail.com>
2024-09-27 08:23:47 -0500

    post

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -1,5 +1,5 @@
 import * as components from './components/indexPadre'
-import  {imageArray} from './components/data-banner1/data'
+import  {imageArray} from './data-banner1/data'
 import './components/post1/post1'
 import './components/footer/footer'
 import './components/banner1/banner1'
