1455dbfbf78b74cd84903f17c342d7fe6dfc0f0e
NikSala10 <salamancanikol10@gmail.com>
2024-11-04 12:28:54 -0500

    file

--- src/components/addPost/addPost.ts
+++ src/components/addPost/addPost.ts
@@ -46,17 +46,17 @@ class AddPost extends HTMLElement  {
                     <div class="write">
                         <input type="text" placeholder="Write the description of the post">
                         <input type="text" placeholder="Write your hashtags">
-                        <input type="image">
                     </div>
                     <div class="img-post">
                         <svg id="galery" xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-image-fill" viewBox="0 0 16 16">
                         <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
                         </svg>
-                        <input type="image">
+                        <input type="file">
                     </div>
                     <hr>
                     <p>Your followers will be able to see the reel in their feed and on your profile.</p>
                 </div>
+                <btn-component color="#361656" label="Add Post"></btn-component>
              </div>
             
             	
