00cb6fd5dc7742ecc86dd2d7a65c71f9880ab09b
NikSala10 <salamancanikol10@gmail.com>
2024-11-17 15:41:35 -0500

    Add styles for user profile section: avatar, name, username, and bio

--- src/screens/account.ts
+++ src/screens/account.ts
@@ -71,7 +71,7 @@ class Account extends HTMLElement {
                         <section class="">
                             <div class="user-info">
                                 <div class="circle-img">
-                                    <img id="img-user" src="../../src/assets/ajenda.jpeg" alt="">
+                                    <img id="img-user" src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
                                 </div>
                                 <div id="follows">
                                     <div id="followers">
