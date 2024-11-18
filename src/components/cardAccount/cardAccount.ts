4aa3950fcfa7dffc6c999d6666f399cae2935a86
NikSala10 <salamancanikol10@gmail.com>
2024-11-17 19:48:12 -0500

    cssFatalV2

--- src/components/cardAccount/cardAccount.ts
+++ src/components/cardAccount/cardAccount.ts
@@ -127,25 +127,19 @@ class CardAccount extends HTMLElement  {
                             <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                             </svg>
-                            <p id="likeCount">${this.likes || this.likes==0 ? this.likes : 'Not found'}</p>
+                            <p id="likeCount">${this.likes || this.likes==0 ? this.likes : 0}</p>
                         </div>
                         <div class="counter" id="comment">
                             <svg class="icon2" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chat-square-fill" viewBox="0 0 16 16">
                                 <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
                             </svg>
-                            <p id="commentCount">${this.comments || this.comments==0 ? this.comments : 'Not found'}</p>
+                            <p id="commentCount">${this.comments || this.comments==0 ? this.comments : 0}</p>
                         </div>
                         <div class="counter" id="favorite">
                             <svg class="icon3" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
                                 <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
                             </svg>
-                            <p id="favoriteCount">${this.favorites || this.favorites==0 ? this.favorites : 'Not found'}</p>
-                        </div>
-                        <div class="counter" id="send">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
-                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
-                            </svg>
-                            <p id="sendCount">${this.send || this.send==0 ? this.send : 'Not found'}</p>
+                            <p id="favoriteCount">${this.favorites || this.favorites==0 ? this.favorites : 0}</p>
                         </div>
                     </div>
                     <div id="comment-post" class"hide">
