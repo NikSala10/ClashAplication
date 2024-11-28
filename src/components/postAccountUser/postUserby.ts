64661d73e1799da9f0c97687313ece27eca5363d
NikSala10 <salamancanikol10@gmail.com>
2024-11-28 17:55:37 -0500

    fix: delete component in postUserby  component

--- src/components/postAccountUser/postUserby.ts
+++ src/components/postAccountUser/postUserby.ts
@@ -122,29 +122,6 @@ class PostUserCard extends HTMLElement  {
             <div class= "card">
                     <hashtags-component hashtags='${this.hashtags ? this.hashtags : ''}'></hashtags-component>
                     <img class="img-post" src="${this.image ? this.image : 'Not found'}">
-                    <div class="post-icons">
-                        <div class="counter" id="like">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-                            </svg>
-                            <p id="likeCount">${this.likes || this.likes==0 ? this.likes : 0}</p>
-                        </div>
-                        <div class="counter" id="comment">
-                            <svg class="icon2" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chat-square-fill" viewBox="0 0 16 16">
-                                <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                            </svg>
-                            <p id="commentCount">${this.commentsN || this.commentsN==0 ? this.commentsN : 0}</p>
-                        </div>
-                        <div class="counter" id="favorite">
-                            <svg class="icon3" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
-                            </svg>
-                            <p id="favoriteCount">${this.favorites || this.favorites==0 ? this.favorites : 0}</p>
-                        </div>
-                    </div>
-                    <div id="comment-post" class"hide">
-                        
-                    </div>
                     
                 </div>	
             `;
