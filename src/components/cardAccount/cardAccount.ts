ddffbd235d361dd9dcae1514e3dbe9f66a5472cd
NikSala10 <salamancanikol10@gmail.com>
2024-11-28 17:54:43 -0500

    fix: delete component in postCard component

--- src/components/cardAccount/cardAccount.ts
+++ src/components/cardAccount/cardAccount.ts
@@ -121,29 +121,6 @@ class CardAccount extends HTMLElement  {
                     <svg id="delete" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 20 20"><path fill="#CAC1E4" d="M8.5 4h3a1.5 1.5 0 0 0-3 0m-1 0a2.5 2.5 0 0 1 5 0h5a.5.5 0 0 1 0 1h-1.054l-1.194 10.344A3 3 0 0 1 12.272 18H7.728a3 3 0 0 1-2.98-2.656L3.554 5H2.5a.5.5 0 0 1 0-1zM9 8a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0zm2.5-.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 1 0V8a.5.5 0 0 0-.5-.5"/></svg>
                     <hashtags-component hashtags='${this.hashtags ? this.hashtags : ''}'></hashtags-component>
                     <img class="img-post" src="${this.image ? this.image : 'Not found'}">
-                    <div class="post-icons">
-                        <div class="counter" id="like">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-                            </svg>
-                            <p id="likeCount">${this.likes || this.likes==0 ? this.likes : 0}</p>
-                        </div>
-                        <div class="counter" id="comment">
-                            <svg class="icon2" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chat-square-fill" viewBox="0 0 16 16">
-                                <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                            </svg>
-                            <p id="commentCount">${this.commentsN || this.commentsN==0 ? this.commentsN : 0}</p>
-                        </div>
-                        <div class="counter" id="favorite">
-                            <svg class="icon3" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
-                            </svg>
-                            <p id="favoriteCount">${this.favorites || this.favorites==0 ? this.favorites : 0}</p>
-                        </div>
-                    </div>
-                    <div id="comment-post" class"hide">
-                        
-                    </div>
                     
                 </div>	
             `;
