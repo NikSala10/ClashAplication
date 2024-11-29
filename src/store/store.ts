648928511d9e65d72194afe6d0e96df4867145eb
NikSala10 <salamancanikol10@gmail.com>
2024-11-29 11:19:11 -0500

    fix: store change initialstate

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,9 +14,9 @@ const onAuth = async () => {
 		
 		if (user) {
 			user.uid !== null ? dispatch(setUserCredentials(user.uid)) : ''; //Es la que se encarga de guardar el id del usuario
-			dispatch(navigate(Screens.ACCOUNT)); //Esta es la de navegar a dashboard
+			dispatch(navigate(Screens.DASHBOARD)); //Esta es la de navegar a dashboard
 		} else {
-			dispatch(navigate(Screens.ACCOUNT));
+			dispatch(navigate(Screens.DASHBOARD));
 		}
 	});
 };
@@ -25,7 +25,7 @@ onAuth();
 
 //El estado global, appState
 const initialState: AppState = {
-	screen: Screens.ACCOUNT,
+	screen: Screens.DASHBOARD,
 	user:  '',
 	userId: '',
 	post: [],
