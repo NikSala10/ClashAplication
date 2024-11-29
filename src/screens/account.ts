8d4fc962a7cac435051598c52add66bc59b915c0
NikSala10 <salamancanikol10@gmail.com>
2024-11-29 08:03:37 -0500

    add new styles for account responsive V2

--- src/screens/account.ts
+++ src/screens/account.ts
@@ -163,8 +163,8 @@ class Account extends HTMLElement {
                             <svg id="logOut" xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12h-9.5m7.5 3l3-3l-3-3m-5-2V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h5a2 2 0 0 0 2-2v-1"/></svg>
                         </div>
                         <div class="filter">
-                            <p id= "Postuser">All <b id="countPost">6</b></p>
-                            <p id= "PostFavorites">Favorites <b id="countPostFavorites">6</b></p>
+                            <p id= "Postuser">All <b id="countPost"></b></p>
+                            <p id= "PostFavorites">Favorites <b id="countPostFavorites"></b></p>
                         </div>
                         <hr>
                         <section class="containers">
